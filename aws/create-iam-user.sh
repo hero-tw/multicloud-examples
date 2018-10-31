5279bbd71724761664519badf101d8a5ac272270
kaiholnes <kaihol42@gmail.com>
2018-10-31 16:08:14 -0400

    Nygel, Kai | Adding users to groups in IAM, print statements

--- aws/create-iam-user.sh
+++ aws/create-iam-user.sh
@@ -5,12 +5,23 @@
 # Create IAM User
 aws iam create-user --user-name "$1" 
 
+#Add
+echo ""
+echo "User will be added to Group: $2"
+echo ""
+aws iam add-user-to-group --user-name "$1" --group-name "$2"
+echo ""
+echo "User's Groups:"
+aws iam list-groups-for-user --user-name "$1"
+
+
 #Generate Credentials
-aws iam create-access-key --user-name "$1" > test2.txt
+aws iam create-access-key --user-name "$1" > accesskeyOutput.txt
 
 #Store Credentials in Param Store
-aws ssm put-parameter --name "$1-SECRET-KEY" --type "SecureString" --value $(cat 'test2.txt' | jq '.AccessKey.SecretAccessKey')
-aws ssm put-parameter --name "$1-SECRET-ID" --type "SecureString" --value $(cat 'test2.txt' | jq '.AccessKey.AccessKeyId')
+aws ssm put-parameter --name "$1-SECRET-KEY" --type "SecureString" --value $(cat 'accesskeyOutput.txt' | jq '.AccessKey.SecretAccessKey')
+aws ssm put-parameter --name "$1-SECRET-ID" --type "SecureString" --value $(cat 'accesskeyOutput.txt' | jq '.AccessKey.AccessKeyId')
 
 #Remove file
-rm test2.txt
+rm accesskeyOutput.txt
+
